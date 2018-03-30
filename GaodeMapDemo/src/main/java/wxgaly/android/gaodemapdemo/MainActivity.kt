c22fc38404024e760a7f757c6db8ebd0a4671a03
wangxuguang <nova_wangxuguang@126.com>
2018-03-30 10:18:09 +0800

    修改高德地图定位问题

--- GaodeMapDemo/src/main/java/wxgaly/android/gaodemapdemo/MainActivity.kt
+++ GaodeMapDemo/src/main/java/wxgaly/android/gaodemapdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package wxgaly.android.gaodemapdemo
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -9,6 +10,7 @@ import com.amap.api.location.AMapLocationClientOption
 import com.amap.api.location.AMapLocationListener
 import com.amap.api.maps.AMap
 import com.amap.api.maps.LocationSource
+import com.amap.api.maps.model.MyLocationStyle
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -29,25 +31,26 @@ class MainActivity : AppCompatActivity(), LocationSource, AMapLocationListener {
     }
 
     private fun location() {
-//        val myLocationStyle = MyLocationStyle()
-        //初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        val myLocationStyle = MyLocationStyle()
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
 //        myLocationStyle.interval(2000) //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-//        aMap?.apply {
-////            myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE)
-//            setMyLocationStyle(myLocationStyle) //设置定位蓝点的Style
-//            uiSettings.isMyLocationButtonEnabled = true //设置默认定位按钮是否显示，非必需设置。
-//            isMyLocationEnabled = true // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
-////            myLocationStyle.showMyLocation(true)
-//            setOnMyLocationChangeListener {
-//                Log.d("wxg", "altitude : ${it.altitude}")
-//                Log.d("wxg", "longitude : ${it.longitude}")
-//            }
-//        }
+        myLocationStyle.strokeColor(Color.RED)
         aMap?.apply {
-            setLocationSource(this@MainActivity)
-            isMyLocationEnabled = true
-            setMyLocationType(AMap.LOCATION_TYPE_LOCATE)
+            //            myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE)
+            setMyLocationStyle(myLocationStyle) //设置定位蓝点的Style
+            uiSettings.isMyLocationButtonEnabled = true //设置默认定位按钮是否显示，非必需设置。
+            isMyLocationEnabled = true // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+//            myLocationStyle.showMyLocation(true)
+            setOnMyLocationChangeListener {
+                Log.d("wxg", "altitude : ${it.latitude}")
+                Log.d("wxg", "longitude : ${it.longitude}")
+            }
         }
+//        aMap?.apply {
+//            setLocationSource(this@MainActivity)
+//            isMyLocationEnabled = true
+////            setMyLocationType(AMap.LOCATION_TYPE_MAP_ROTATE)
+//        }
 
     }
 
@@ -80,11 +83,13 @@ class MainActivity : AppCompatActivity(), LocationSource, AMapLocationListener {
         mListener?.apply {
             location?.apply {
                 if (errorCode == 0) {
-                    Log.d("wxg", "location error")
-                } else {
                     Log.d("wxg", "latitude : $latitude")
                     Log.d("wxg", "longitude : $longitude")
                     mListener?.onLocationChanged(location)
+
+                } else {
+                    Log.d("wxg", "location error-- $errorCode ----$errorInfo")
+
                 }
             }
         }
@@ -107,6 +112,7 @@ class MainActivity : AppCompatActivity(), LocationSource, AMapLocationListener {
         super.onDestroy()
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
         mapView.onDestroy()
+        mlocationClient?.onDestroy()
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
